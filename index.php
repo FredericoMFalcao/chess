880e6f4908c86c169b67a6b6e1388c7cede2a9db
Frederico Falcao <fredericomfalcao@hotmail.com>
2017-08-21 11:34:36 +0100

    better organized folder structure

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="/jQuery.js"></script>
+	<script src="/front-end/jQuery.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
 
 	<style>
