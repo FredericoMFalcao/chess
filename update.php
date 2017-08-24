76ace8ca2b5645eccda0503092bcd547ff4402bb
Frederico <FredericoMFalcao@hotmail.com>
2017-08-24 15:13:19 +0100

    Fixed webhook script

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
 header("Content-type: text/plain");
-passthru("GIT_WORK_TREE=/var/www/chessgame git pull");
-passthru("cd /var/www/chessgame && make");
+passthru("GIT_WORK_TREE=/var/www/chessgame GIT_DIR=/var/www/chessgame/.git git pull");
+passthru("cd /var/www/chessgame/back-end && make");
 
